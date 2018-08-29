97b15534a90190411afeff0df0c7e0eb70211b87
Viet-Vo <viet.allen.qvo@gmail.com>
2018-08-29 10:24:15 -0700

    first in class update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,29 +22,43 @@ var exampleRate = "3500";
 
 var name = exampleName;
 var email = exampleEmail;
-var date = exampleDate;
+var startDate = exampleDate;
 var rate = exampleRate;
 
-var rowH = ""
-database.ref("/bidderData").on("value", function(snap) {
-    name = snap.val().name;
-    email = snap.val().email;
-    date = snap.val().date;
-    rate = snap.val().rate;
-    $
+var rowH = "";
+
+database.ref("/businessData").on("value", function (snap) {
+    if (snapshot.child("name").exists() && snapshot.child("email").exists() && snapshot.child("date").exists() && snapshot.child("rate").exists()) {
+        name = snap.val().name;
+        email = snap.val().email;
+        startDate = snap.val().date;
+        rate = snap.val().rate;
+        $("#test1").test(snap.val().name);
+        $("#test2").test(snap.val().email);
+        $("#test3").test(snap.val().date);
+        $("#test4").test(snap.val().rate);
+    }
+});
+
+$(".btn").on("click", function() {
+    var name = $("#userInput1").val().trim();
+    var email = $("#userInput2").val().trim();
+    var startDate = $("#userInput3").val().trim();
+    var rate = $("#userInput4").val().trim();
+    console.log(name);
+    console.log(email);
+    console.log(startDate);
+    console.log(rate);
 });
 
 
 
-
-
-connectedRef.on("value", function(snap) {
+connectedRef.on("value", function (snap) {
     if (snap.val()) {
         var con = connectionRef.push(true);
         con.onDisconnect().remove();
     }
 });
-connectionsRef.on("value", function(snap)) {
+connectionsRef.on("value", function (snap)) {
     console.log(snap.numChildren());
 }
-
