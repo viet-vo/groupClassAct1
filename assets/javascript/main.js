01831f8668f90ea535c208a0c3cc5a5c6f1e6d96
Viet-Vo <viet.allen.qvo@gmail.com>
2018-08-28 19:28:36 -0700

    initial, alt index

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyB4Ooeb3AO3GUZ0PlheNtyy2jhedAaIuIQ",
+    authDomain: "firstproject-vqv.firebaseapp.com",
+    databaseURL: "https://firstproject-vqv.firebaseio.com",
+    projectId: "firstproject-vqv",
+    storageBucket: "firstproject-vqv.appspot.com",
+    messagingSenderId: "578400054546"
+  };
+  firebase.initializeApp(config);
